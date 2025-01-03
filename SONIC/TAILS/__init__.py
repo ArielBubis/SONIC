ca44c8bdacdde114159241f6ae6f4ab5e1511eac
ArielBubis <arielbubis@gmail.com>
2025-01-03 20:22:15 +0200

    Add EncodecMAEEmbedder and MusicNNEmbedder classes; update imports in __init__.py and main.py

--- SONIC/TAILS/__init__.py
+++ SONIC/TAILS/__init__.py
@@ -2,4 +2,5 @@ from TAILS import mfcc as MFCC
 from TAILS import vit as ViT
 from TAILS import embedder as embedder
 from TAILS import musicfm as MusicFM
-# from . import musicnn as MusiCNN
+from TAILS import musicnn as MusiCNN
+from TAILS import emae as EncodecMAE
