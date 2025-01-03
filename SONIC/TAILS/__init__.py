e766c39931e895d73f69f6ec63911ddc15c67b9c
ArielBubis <arielbubis@gmail.com>
2025-01-03 10:43:49 +0200

    Add MusicFMEmbedder class and update import paths for MusicFM embeddings

--- SONIC/TAILS/__init__.py
+++ SONIC/TAILS/__init__.py
@@ -1,7 +1,8 @@
-from . import mfcc as MFCC
-from . import vit as ViT
+from TAILS import mfcc as MFCC
+from TAILS import vit as ViT
 from . import m2v as M2V
 from . import mert as MERT
-from . import embedder as embedder
+from TAILS import embedder as embedder
 from . import encoder as encoder
+from TAILS import musicfm as MusicFM
 # from . import musicnn as MusiCNN
