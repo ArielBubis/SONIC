d45ff44b1e7abbec1db463c7d584c5d4f48f15a6
Calibourne <Eddiekanevsky@gmail.com>
2025-01-04 11:47:21 +0200

    fixed import error in TAILS __init__

--- SONIC/TAILS/__init__.py
+++ SONIC/TAILS/__init__.py
@@ -1,9 +1,8 @@
-from TAILS import mfcc as MFCC
-from TAILS import vit as ViT
+from . import embedder as embedder
+from . import encoder as encoder
+from . import mfcc as MFCC
+from . import musicfm as MusicFM
+from . import emae as EncodecMAE
 from . import m2v as M2V
 from . import mert as MERT
-from TAILS import embedder as embedder
-from . import encoder as encoder
-from TAILS import musicfm as MusicFM
-from TAILS import musicnn as MusiCNN
-from TAILS import emae as EncodecMAE
+from . import vit as ViT                                                                                                                                                                                                                                                                                                                                                                                                        
