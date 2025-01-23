4e8b3d97786de49e328d480b729d2c88b466264a
ArielBubis <arielbubis@gmail.com>
2025-01-23 11:21:34 +0200

    refactor SNN calculation to support dynamic recommendation lengths and optional normalization

--- SONIC/ROUGE/snn.py
+++ SONIC/ROUGE/snn.py
@@ -49,7 +49,7 @@ class ShallowEmbeddingModel(nn.Module):
         scores = self.cossim(user_embeds, item_embeds)
         return scores
 
-    def extract_embeddings(self):
+    def extract_embeddings(self, normalize=True):
         user_embeddings = self.user_embeddings.weight.data
         item_embeddings = self.item_embeddings.weight.data
 
@@ -57,8 +57,9 @@ class ShallowEmbeddingModel(nn.Module):
             user_embeddings = self.model(user_embeddings).cpu().numpy()
             item_embeddings = self.model(item_embeddings).cpu().numpy()
 
-        user_embeddings = user_embeddings / np.linalg.norm(user_embeddings, axis=1, keepdims=True)
-        item_embeddings = item_embeddings / np.linalg.norm(item_embeddings, axis=1, keepdims=True)
+        if normalize:
+            user_embeddings = user_embeddings / np.linalg.norm(user_embeddings, axis=1, keepdims=True)
+            item_embeddings = item_embeddings / np.linalg.norm(item_embeddings, axis=1, keepdims=True)
 
         return user_embeddings, item_embeddings
 
@@ -98,8 +99,8 @@ def prepare_data(train, val, test, mode='val'):
     user_history = train.groupby('user_id', observed=False)['item_id'].agg(set).to_dict()
     return train, val, user_history, ie
 
-def calc_snn(model_name, train, val, test, mode='val', emb_dim_out=300):
-    run_name = f'{model_name}_snn'
+def calc_snn(model_name, train, val, test, mode='val', suffix='cosine', k=50, emb_dim_out=300):
+    run_name = f'{model_name}_{suffix}'
     train, val, user_history, ie = prepare_data(train, val, test, mode)
     item_embs = load_embeddings(model_name, train, ie)
 
@@ -108,28 +109,40 @@ def calc_snn(model_name, train, val, test, mode='val', emb_dim_out=300):
     emb_dim_in = item_embs.shape[1]
 
     model = ShallowEmbeddingModel(num_users, num_items, emb_dim_in, precomputed_item_embeddings=item_embs, emb_dim_out=emb_dim_out)
-    user_embs, item_embs = model.extract_embeddings()
+    user_embs, item_embs = model.extract_embeddings(normalize=(suffix == 'cosine'))
 
     all_users = val.user_id.unique()
 
-    user_recommendations = {}
-    for user_id in tqdm(all_users, desc=f'applying snn for {model_name}'):
-        history = user_history[user_id]
+    if isinstance(k, int):
+        k = [k]
+    max_k = max(k)
+
+    # Precompute recommendations up to max_k
+    user_recommendations_maxk = {}
+    for user_id in tqdm(all_users, desc=f'Generating recommendations for {model_name}'):
+        history = user_history.get(user_id, set())
         user_vector = user_embs[user_id]
         scores = np.dot(item_embs, user_vector)
         recommendations = np.argsort(scores)[::-1]
-        recommendations = [idx for idx in recommendations if idx not in history][:50]
-        user_recommendations[user_id] = recommendations
+        # Filter out history and take up to max_k
+        filtered_recommendations = [idx for idx in recommendations if idx not in history][:max_k]
+        user_recommendations_maxk[user_id] = filtered_recommendations
 
-    df = dict_to_pandas(user_recommendations)
+    all_metrics_val = []
+    for current_k in k:
+        user_recommendations = {}
+        for user_id in all_users:
+            user_recommendations[user_id] = user_recommendations_maxk[user_id][:current_k]
+        df = dict_to_pandas(user_recommendations)
+        metrics_val = calc_metrics(val, df, current_k)
+        metrics_val = metrics_val.apply(mean_confidence_interval)
+        all_metrics_val.append(metrics_val)
 
+    metrics_val_concat = pd.concat(all_metrics_val, axis=0)
     os.makedirs('metrics', exist_ok=True)
-    metrics_val = calc_metrics(val, df, 50)
-    metrics_val = metrics_val.apply(mean_confidence_interval)
+    metrics_val_concat.to_csv(f'metrics/{run_name}_val.csv')
 
-    metrics_val.to_csv(f'metrics/{run_name}_val.csv')
-
-    return metrics_val
+    return metrics_val_concat
 
 def snn(model_names, suffix, k, mode='val', emb_dim_out=300):
     train = pd.read_parquet('data/train.pqt')
@@ -137,6 +150,6 @@ def snn(model_names, suffix, k, mode='val', emb_dim_out=300):
     test = pd.read_parquet('data/test.pqt')
     
     if isinstance(model_names, str):
-        return calc_snn(model_names, train, val, test, mode, emb_dim_out)
+        return calc_snn(model_names, train, val, test, mode, suffix, k, emb_dim_out)
     else:
-        return pd.concat([calc_snn(model_name, train, val, test, mode, emb_dim_out) for model_name in model_names])
+        return pd.concat([calc_snn(model_name, train, val, test, mode, suffix, k, emb_dim_out) for model_name in model_names])
